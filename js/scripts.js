1545ac0594fc91ae194ac9cbf3c5f8f36a7abb43
Chris kuiper and paul hess <chris.kuiper@gmail.com>
2016-01-14 15:48:03 -0800

    add content to JS pages"

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,19 @@
 $(document).ready(function(){
   $(".inline").text("dynamic style(this text was inserterd using javascript and css on a span element)");
 
+
+  $('button#function').click(function(){
+    alert("Hello World!");
+  });
+
+  $('h4#jqfunction').click(function() {
+    alert("This is a header.");
+  });
+
+  $("#formexample form").submit(function(event) {
+    var name = $("input#you").val();
+    alert("Hello " + name + "!");
+    event.preventDefault();
+  });
+
 });
